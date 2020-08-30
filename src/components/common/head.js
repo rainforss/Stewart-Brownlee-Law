e7d2f5a18d0d69c3e848eb7ce91f2fa765f6cd7b
rainforss <shengyan@ualberta.ca>
2020-08-29 21:10:11 -0600

    touch up

--- src/components/common/head.js
+++ src/components/common/head.js
@@ -12,6 +12,22 @@ const Head = ({ title }) => {
       }
     }
   `)
-  return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+  return (
+    <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+      <meta property="og:title" content="Stewart Brownlee Law" />
+      <meta
+        property="og:image"
+        content="https://dreamy-hawking-5cf65d.netlify.app/home.png"
+      />
+      <meta
+        property="og:description"
+        content="Alberta law office based in North Edmonton focusing on litigations"
+      />
+      <meta
+        property="og:url"
+        content="https://dreamy-hawking-5cf65d.netlify.app/"
+      />
+    </Helmet>
+  )
 }
 export default Head
