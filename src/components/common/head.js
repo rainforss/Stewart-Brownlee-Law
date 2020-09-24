d77b9d4152c971ede33b34be1df2c571af47652f
rainforss <shengyan@ualberta.ca>
2020-09-24 00:57:43 -0600

    Modified meta tags for better SEO, added website thumbnail

--- src/components/common/head.js
+++ src/components/common/head.js
@@ -14,15 +14,23 @@ const Head = ({ title }) => {
   `)
   return (
     <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
-      <meta property="og:title" content="Stewart Brownlee Law" />
       <meta
+        name="description"
+        content="Alberta law office based in north Edmonton focusing on litigations"
+      />
+      <meta name="title" property="og:title" content="Stewart Brownlee Law" />
+      <meta property="og:type" content="Website" />
+      <meta
+        name="image"
         property="og:image"
-        content="https://dreamy-hawking-5cf65d.netlify.app/home.png"
+        content="https://res.cloudinary.com/rainforss/image/upload/v1600929593/stewartbrownleelaw_xdspsh.png"
       />
       <meta
+        name="description"
         property="og:description"
         content="Alberta law office based in North Edmonton focusing on litigations"
       />
+      <meta name="author" content="Jake Chen" />
       <meta
         property="og:url"
         content="https://dreamy-hawking-5cf65d.netlify.app/"
